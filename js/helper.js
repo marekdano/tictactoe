a226774dd7c3cb7001b0dad2edbb919fd52eae40
Marek Dano <mk.dano@gmail.com>
2014-02-12 23:06:05 +0000

    Added Game object to avoid using global variables, color changed on hovering of buttons

--- js/helper.js
+++ js/helper.js
@@ -1,212 +1,219 @@
 "use strict";
 
-// initialize an empty board;
-var board = [0,0,0,0,0,0,0,0,0];
-// all winning combinations 
-var WIN_COMBS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
-		  [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
-		  [0, 4, 8], [2, 4, 6]];        // diagonal
-// best moves in order
-var BEST_MOVES = [4, 0, 2, 6, 8, 1, 3, 5, 7];
-// winning player of the game
-var winningPlayer;
-// Two players play the game. First player starts first.
-var turnPlayer;
- // two players or player against computer
-var player1, player2, computer;
-
 var Player = function(name) {
-	this.name = name;
-  	this.score = 0;
+  this.name = name;
+  this.score = 0;
 };
 
 Player.prototype.won = function() {
-	this.score += 1;
+  this.score += 1;
 };
 
-// check the cell if it's empty (with value O)
-var checkCell = function(index) {
-	return board[index];
-};
+var Game = {
+  // initialize an empty board;
+  board: [0,0,0,0,0,0,0,0,0],
+  // all winning combinations 
+  WIN_COMBS: [[0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+		          [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+		          [0, 4, 8], [2, 4, 6]],        // diagonal
+  //best moves in order
+  BEST_MOVES: [4, 0, 2, 6, 8, 1, 3, 5, 7],
+  // winning player of the game
+  winningPlayer: undefined,
+  // Two players play the game. First player starts first.
+  turnPlayer: undefined,
+  // two players or player against computer
+  player1: undefined, 
+  player2: undefined, 
+  computer: undefined,
 
-var getWinningPlayer = function() {
-	return winningPlayer;
-};
+  // check the cell if it's empty (with value O)
+  checkCell: function(index) {
+    return this.board[index];
+  },
 
-// a player move
-var move = function(cell, index) {
- 	if(turnPlayer === player1) {
-   		$(cell).addClass("first_player_cell");
-  	} else if(turnPlayer === player2) {
-   		$(cell).addClass("second_player_cell");
-  	} 
-   
-	// keep record which player moved in particular cell
-	board[index] = turnPlayer;
-};
+  getWinningPlayer: function() {
+  	return this.winningPlayer;
+  },
 
-// Check for the best computer move  
-// First add a computer move to empty field and check if computer can win. 
-// If yes, move computer there.
-// Second add a human move to empty field and check if human can win . If yes, 
-// move computer there.
-// If no move in the combination of moves for computer and human is found, 
-// move computer in one of the best moves.   
-var computerMove = function() {
-	var help_board = board;
-  
-  	// find if computer can win by pretending a computer move
-  	for(var i=0; i<help_board.length; i++) {
-   		if(help_board[i] === 0) {
-			// checking if computer can win
-			help_board[i] = computer;
-			if(checkMove(help_board)) {
-	 			board[i] = turnPlayer;
-	 			$(".board_cell[id=" + i + "]").addClass("second_player_cell");
-	 			return;
-			}
-			help_board[i] = 0;
-   		}
-  	} 
+  getComputer: function() {
+    return this.computer;
+  },
 
-  	// find if human can win by pretending a player1 (human) move
-  	for(var j=0; j<help_board.length; j++) {
-   		if(help_board[j] === 0) {
-		// checking if player1 (human) can win
-			help_board[j] = player1;
-			if(checkMove(help_board)) {
-		 		board[j] = turnPlayer;
-		 		$(".board_cell[id=" + j + "]").addClass("second_player_cell");
-		 		return;
-			}
-			help_board[j] = 0;
-   		}
-  	} 
+  // a player move
+  move: function(cell, index) {
+   	if(this.turnPlayer === this.player1) {
+     	$(cell).addClass("first_player_cell");
+    } else if(this.turnPlayer === this.player2) {
+     	$(cell).addClass("second_player_cell");
+    } 
+     
+  	// keep record which player moved in particular cell
+  	this.board[index] = this.turnPlayer;
+  },
 
-  	// move computer in one of the best moves
-  	for (var k=0; k<BEST_MOVES.length; k++) {
-   		if(board[BEST_MOVES[k]] === 0) {
-			board[BEST_MOVES[k]] = turnPlayer;
-			$(".board_cell[id=" + BEST_MOVES[k] + "]").addClass("second_player_cell");
-			//console.log(turnPlayer['name']);
-			return;
-   		}
+  // Check for the best computer move  
+  // First add a computer move to empty field and check if computer can win. 
+  // If yes, move computer there.
+  // Second add a human move to empty field and check if human can win . If yes, 
+  // move computer there.
+  // If no move in the combination of moves for computer and human is found, 
+  // move computer in one of the best moves.   
+  computerMove: function() {
+  	var help_board = this.board;
+    
+    // find if computer can win by pretending a computer move
+    for(var i=0; i<help_board.length; i++) {
+     	if(help_board[i] === 0) {
+  		  // checking if computer can win
+  		  help_board[i] = this.computer;
+  		  if(this.checkMove(help_board)) {
+  	 		  this.board[i] = this.turnPlayer;
+  	 		  $(".board_cell[id=" + i + "]").addClass("second_player_cell");
+  	 		  return;
+  		  }
+  		  help_board[i] = 0;
+     	}
+    } 
+
+    // find if human can win by pretending a player1 (human) move
+    for(var j=0; j<help_board.length; j++) {
+     	if(help_board[j] === 0) {
+  		  // checking if player1 (human) can win
+  			help_board[j] = this.player1;
+  			if(this.checkMove(help_board)) {
+  		 		this.board[j] = this.turnPlayer;
+  		 		$(".board_cell[id=" + j + "]").addClass("second_player_cell");
+  		 		return;
+  			}
+  			help_board[j] = 0;
+     	}
+    } 
+
+    // move computer in one of the best moves
+    for (var k=0; k<this.BEST_MOVES.length; k++) {
+     	if(this.board[this.BEST_MOVES[k]] === 0) {
+  			this.board[this.BEST_MOVES[k]] = this.turnPlayer;
+  			$(".board_cell[id=" + this.BEST_MOVES[k] + "]").addClass("second_player_cell");
+  			return;
+     	}
+    }
+  },
+
+
+  // check if a new move is the winning
+  checkMove: function(board) {
+    var WIN_COMBS = this.WIN_COMBS;
+  	for(var i=0; i<WIN_COMBS.length; i++) {
+  	    if(board[WIN_COMBS[i][0]] === board[WIN_COMBS[i][1]] && board[WIN_COMBS[i][0]] === board[WIN_COMBS[i][2]] && 
+  			board[WIN_COMBS[i][1]] === board[WIN_COMBS[i][2]] && board[WIN_COMBS[i][1]]!== 0) {
+  		
+  		    return true;
+  	   	}
   	}
- };
-
-
-// check if a new move is the winning
-var checkMove = function(board) {
-	for(var i=0; i<WIN_COMBS.length; i++) {
-	   	if(board[WIN_COMBS[i][0]] === board[WIN_COMBS[i][1]] && board[WIN_COMBS[i][0]] === board[WIN_COMBS[i][2]] && 
-			board[WIN_COMBS[i][1]] === board[WIN_COMBS[i][2]] && board[WIN_COMBS[i][1]]!== 0) {
-		
-		return true;
-	   	}
-	}
-	return false;
-};
-
-// return next player turn 
-var nextPlayer = function() {
-  	if (computer !== undefined) {
-   		turnPlayer = (turnPlayer === player1) ? computer : player1;
-  	} else {
-   		turnPlayer = (turnPlayer === player1) ? player2 : player1;
-  	} 
-};
-
-// Check if there is a winner
-// If yes, return true. If not, return false
-// If there is no winner and there is no more moves available,
-// return the string tie.
-var checkWinner = function(board) {
-	if(checkMove(board)) {
-		return true;
-	}
-
-  	// the whole board is filled, no more moving available
-  	if(board.indexOf(0) === -1){
-	  	return "tie";
-  	}
-
-  	// no winner found
   	return false;
-};
+  },
 
-// Displays the result of the game (winner name, tie) and set up a new game
-var endGame = function(result) {
-	if (result !== "tie") {
-   		winningPlayer = turnPlayer;
-   		alert(winningPlayer['name'] + " win!!! Congratulation!!!");
-   		winningPlayer.won();
-  	} else {
-   		alert("It's a tie! :)");
+  // return next player turn 
+  nextPlayer: function() {
+    // if computer is defined, game between computer and human
+    if (this.computer) {
+     	this.turnPlayer = (this.turnPlayer === this.player1) ? this.computer : this.player1;
+    } else {
+     	this.turnPlayer = (this.turnPlayer === this.player1) ? this.player2 : this.player1;
+    } 
+  },
+
+  // Check if there is a winner
+  // If yes, return true. If not, return false
+  // If there is no winner and there is no more moves available,
+  // return the string tie.
+  checkWinner: function(board) {
+  	if(this.checkMove(this.board)) {
+  		return true;
   	}
 
-  	return nextGame();
-};
+    // the whole board is filled, no more moving available
+    if(this.board.indexOf(0) === -1){
+  	  return "tie";
+    }
 
-// reset the board
-var resetBoard = function() {
-	for(var i=0; i<board.length; i++) {
-   		$(".board_cell[id=" + i + "]").removeClass("first_player_cell second_player_cell");
-   		board[i] = 0;
+    // no winner found
+    return false;
+  },
+
+  // Displays the result of the game (winner name, tie) and set up a new game
+  end: function(result) {
+  	if (result !== "tie") {
+     	this.winningPlayer = this.turnPlayer;
+     	alert(this.winningPlayer['name'] + " wins!!! Congratulation!!!");
+     	this.winningPlayer.won();
+    } else {
+     	alert("It's a tie! :)");
+    }
+
+    return this.nextGame();
+  },
+
+  // reset the board
+  resetBoard: function() {
+  	for(var i=0; i<this.board.length; i++) {
+     	$(".board_cell[id=" + i + "]").removeClass("first_player_cell second_player_cell");
+     	this.board[i] = 0;
+    }
+    
+    // first player always starts the game as first
+    this.turnPlayer = this.player1;
+  },
+
+  // reset the board, update score, set first move for player1, winning player is undefined
+  nextGame: function() {
+  	this.resetBoard();
+    // update the score
+    $("input[name=playerOne]").val(this.player1.score);
+    // if player2 is defined, the player2 score is displayed
+    this.player2 && $("input[name=playerTwo]").val(this.player2.score);
+    // if computer is defined, the player2 score is displayed
+    this.computer && $("input[name=playerTwo]").val(this.computer.score);
+    // set winning player to undefined
+    this.winningPlayer = undefined;
+    // first player starts the game first
+    this.turnPlayer = this.player1;
+  },
+
+  // Initialize a new game every time when the page is loaded or the buttons 
+  // (two players or computer) are hit 
+  initNew: function(button) { 
+  	if (button === "btn-two_players") {
+     	this.player2 = new Player("Second player");
+     	$("h3#second_player").html("Player 2")
+     	$("h1#main_heading").html("tic tac toe for 2");
+     	this.computer = undefined;
+    } else if (button === "btn-computer") {
+     	this.computer = new Player("Computer");
+     	$("h1#main_heading").html("tic tac toe against computer");
+     	$("h3#second_player").html("Computer");
+     	this.player2 = undefined;
+    }
+    this.player1 = new Player("First player");
+    $("input[name=playerOne]").val(this.player1.score);
+    // if player2 is defined, the player2 score is displayed
+    this.player2 && $("input[name=playerTwo]").val(this.player2.score);
+    // if computer is defined, the player2 score is displayed
+    this.computer && $("input[name=playerTwo]").val(this.computer.score); 
+    
+    return this.nextGame();
   }
-  
-  // first player starts the game first
-    turnPlayer = player1;
-};
-
-// reset the board, update score, set first move for player1, winning player is undefined
-var nextGame = function() {
-	resetBoard();
-  	// update the score
-  	$("input[name=player1]").val(player1.score);
-  	if (player2 !== undefined) { 
-   		$("input[name=player2]").val(player2.score); 
-  	} else if (computer !== undefined) {
-   		$("input[name=player2]").val(computer.score);
-  	} 
-  	// set winning player to undefined
-  	winningPlayer = undefined;
-  	// first player starts the game first
-  	turnPlayer = player1;
-};
-
-// Initialize a new game every time when the page is loaded or the buttons 
-// (two players or computer) are hit 
-var initNewGame = function(button) { 
-	if (button === "btn-two_players") {
-   		player2 = new Player("Second player");
-   		$("h3#second_player").html("Player 2")
-   		$("h1#main_heading").html("tic tac toe for 2");
-   		computer = undefined;
-  	} else if (button === "btn-computer") {
-   		computer = new Player("Computer");
-   		$("h1#main_heading").html("tic tac toe against computer");
-   		$("h3#second_player").html("Computer");
-   		player2 = undefined;
-  	}
-  	player1 = new Player("First player");
-  	$("input[name=player1]").val(player1.score);
-  	if (player2 !== undefined) {
-   		$("input[name=player2]").val(player2.score);
-  	} else if (computer !== undefined) {
-   		$("input[name=player2]").val(computer.score); 
-  	}
-  
-  	return nextGame();
-};
+}
 
 
 $(document).ready(function() {
-
- 	initNewGame("btn-two_players");
+  // initialize a new game when the page is loaded
+ 	Game.initNew("btn-two_players");
 
  	// get action when hovering over board cells
  	$(".board_cell").hover(function () {
-   		$(this).addClass("cell_hovered");
+    $(this).addClass("cell_hovered");
 	}, function () {
 		$(this).removeClass("cell_hovered");
 	});
@@ -214,29 +221,29 @@ $(document).ready(function() {
  	// if one of two buttons are hit initialize a new game 
 	$("button").click(function() {
 		var buttonName = $(this).attr('id');
-	 	(buttonName === "btn-reset") ? resetBoard() : initNewGame(buttonName); 
+	 	(buttonName === "btn-reset") ? Game.resetBoard() : Game.initNew(buttonName); 
 	});
 
  	// get the appropriate action when the cell is clicked
 	$(".board_cell").click(function() {
-  		var index = parseInt($(this).attr('id'));
-  		// check if cells are available until someone wins
-  		if(checkCell(index) === 0 && getWinningPlayer() === undefined) {
-   			move(this, index);
-   			var result = checkWinner(board);
-   			if (result || result === "tie") {
-				return endGame(result);
-   			} else {
-				nextPlayer();
-				if(computer !== undefined) {
-	 				computerMove();
-	 				result = checkWinner(board);
+  	var index = parseInt($(this).attr('id'));
+  	// check if cells are available until someone wins
+  	if(Game.checkCell(index) === 0 && Game.getWinningPlayer() === undefined) {
+   		Game.move(this, index);
+   		var result = Game.checkWinner(board);
+   		if (result || result === "tie") {
+				return Game.end(result);
+   		} else {
+        Game.nextPlayer();
+        if(Game.getComputer()) {
+	 				Game.computerMove();
+	 				result = Game.checkWinner(board);
 	 				if (result || result === "tie") {
-	  					return endGame(result);
+	  				return Game.end(result);
 	 				}
-	 				nextPlayer();
+	 				Game.nextPlayer();
 				}
-   			}
-  		}
+   		}
+  	}
  	});
 });
